8cd03a27fa570b840447a836fd7eac6afe7a1560
DanilLozhkin <117292325+DanilLozhkin@users.noreply.github.com>
2023-05-03 13:23:33 +0300

    итог всей работы

--- js/main.js
+++ js/main.js
@@ -6,29 +6,40 @@ const path = require('path');
 router.use(express.static('public'));
 
 
-router.get('/models/normalize.css', (req, res) => {
-    const filePath = path.join(__dirname, '../public/normalize.css');
-    res.sendFile(filePath);
+router.get('/models/normalize.css', (req, res, next) => {
+    try {
+        const filePath = path.join(__dirname, '../public/normalize.css');
+        res.sendFile(filePath);
+    } catch (error) {
+        next(new Error('Ошибка при отправке файла normalize.css: ' + error.message));
+    }
 });
 
-router.get('/models/style_2.css', (req, res) => {
-    const filePath = path.join(__dirname, '../public/style_2.css');
-    res.sendFile(filePath);
+router.get('/models/style_2.css', (req, res, next) => {
+    try {
+        const filePath = path.join(__dirname, '../public/style_2.css');
+        res.sendFile(filePath);
+    } catch (error) {
+        next(new Error('Ошибка при отправке файла style_2.css: ' + error.message));
+    }
 });
 
-router.get('/models/three.js', (req, res) => {
-    const filePath = path.join(__dirname, '../public/three.js');
-    res.sendFile(filePath);
+router.get('/models/three.js', (req, res, next) => {
+    try {
+        const filePath = path.join(__dirname, '../public/three.js');
+        res.sendFile(filePath);
+    } catch (error) {
+        next(new Error('Ошибка при отправке файла three.js: ' + error.message));
+    }
 });
 
-router.get('/models/main_7.js', (req, res) => {
-    const filePath = path.join(__dirname, '../public/main_7.js');
-    res.sendFile(filePath);
-});
-
-router.get('/models/quer_1.js', (req, res) => {
-    const filePath = path.join(__dirname, '../public/quer_1.js');
-    res.sendFile(filePath);
+router.get('/models/main_7.js', (req, res, next) => {
+    try {
+        const filePath = path.join(__dirname, '../public/main_7.js');
+        res.sendFile(filePath);
+    } catch (error) {
+        next(new Error('Ошибка при отправке файла main_7.js: ' + error.message));
+    }
 });
 
 
--- public/main_7.js
+++ public/main_7.js
@@ -1,4 +1,18 @@
 
+const button = document.getElementById('but');
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    const typ = document.getElementById('p3').value;
+
+    const vertexElement = document.getElementById('p4');
+    const vertex = vertexElement.value.split(',').map(Number);
+
+    const colorElement = document.getElementById('p5');
+    const Color = colorElement.value;
+    
+    TREE_JS(typ, vertex, Color)
+});
 
 
 function TREE_JS(typ, vertex, Color) {
